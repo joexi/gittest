869924b8b58e1a23503b175e83856fda26ae71b6
骏飞 奚 <junfeixi@junfeideMacBook-Pro.local>
2016-01-08 13:43:01 +0800

    init=================

--- test/Assets/NewBehaviourScript1.cs
+++ test/Assets/NewBehaviourScript1.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +12,4 @@ public class NewBehaviourScript : MonoBehaviour {
 	void Update () {
 	
 	}
-
 }
